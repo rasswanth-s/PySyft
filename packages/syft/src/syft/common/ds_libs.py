cd0e58f7bafcf887943917fbbf7a78d8ed1cc3e8
Ionesio Junior <ionesiojr@gmail.com>
2022-11-16 10:55:47 -0300

    CREATE a file to centralize data science package imports

--- packages/syft/src/syft/common/ds_libs.py
+++ packages/syft/src/syft/common/ds_libs.py
@@ -1,9 +1,15 @@
 # third party
-import jaxlib
-import numpy
-from numpy.typing import ArrayLike
-import pandas
-import pyarrow
-import torch
-from torch import device
-from torch.nn import Parameter
+import jaxlib  # noqa: F401
+import numpy  # noqa: F401
+from numpy.random import randint  # noqa: F401
+from numpy.typing import ArrayLike  # noqa: F401
+from numpy.typing import NDArray  # noqa: F401
+import pandas  # noqa: F401
+from pandas import DataFrame  # noqa: F401
+import pyarrow  # noqa: F401
+from scipy.ndimage.interpolation import rotate  # noqa: F401
+from scipy.optimize import minimize_scalar  # noqa: F401
+from scipy.optimize import shgo  # noqa: F401
+import torch  # noqa: F401
+from torch import device  # noqa: F401
+from torch.nn import Parameter  # noqa: F401
